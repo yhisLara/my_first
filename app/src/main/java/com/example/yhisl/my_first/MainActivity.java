f43e6a3c650055bfbdc2135711303b77260af72f
YhisLara <yhlara@alumnos.ubiobio.cl>
2016-11-26 00:22:10 -0300

    Lyfecicles borrados

--- app/src/main/java/com/example/yhisl/my_first/MainActivity.java
+++ app/src/main/java/com/example/yhisl/my_first/MainActivity.java
@@ -14,29 +14,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
     }
-        @Override protected void onStart(){
-            super.onStart();
-            Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
-        }
-        @Override protected void onResume(){
-            super.onResume();
-            Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
-        }
-        @Override protected void onPause(){
-            super.onPause();
-            Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
-        }
-        @Override protected void onStop(){
-            super.onStop();
-            Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
-        }
-        @Override protected void onRestart(){
-            super.onRestart();
-            Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
-        }
-        @Override protected void onDestroy(){
-            Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
-            super.onDestroy();
-
-        }
     }
