ba9776fda6cc0d3972454903c73ec9b4ee531126
YhisLara <yhlara@alumnos.ubiobio.cl>
2016-11-26 23:22:01 -0300

    Integración superToast librery en gradle

--- app/src/main/java/com/example/yhisl/my_first/MainActivity.java
+++ app/src/main/java/com/example/yhisl/my_first/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.yhisl.my_first;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.github.johnpersano.supertoasts.library.Style;
+import com.github.johnpersano.supertoasts.library.SuperActivityToast;
+import com.github.johnpersano.supertoasts.library.utils.PaletteUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+
+        SuperActivityToast.create(this, new Style(), Style.TYPE_BUTTON)
+                .setButtonText("UNDO")
+                .setOnButtonClickListener("good_tag_name", null, null)
+                .setProgressBarColor(Color.WHITE)
+                .setText("Email deleted")
+                .setDuration(Style.DURATION_LONG)
+                .setFrame(Style.FRAME_LOLLIPOP)
+                .setColor(PaletteUtils.getSolidColor(PaletteUtils.MATERIAL_PURPLE))
+                .setAnimations(Style.ANIMATIONS_POP).show();
     }
 
     }
