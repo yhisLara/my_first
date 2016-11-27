457348430b62f1c6bd2504ab392f920bc59d92b8
YhisLara <yhlara@alumnos.ubiobio.cl>
2016-11-27 00:09:29 -0300

    trying interface UI

--- app/src/main/java/com/example/yhisl/my_first/MainActivity.java
+++ app/src/main/java/com/example/yhisl/my_first/MainActivity.java
@@ -18,15 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SuperActivityToast.create(this, new Style(), Style.TYPE_BUTTON)
-                .setButtonText("UNDO")
-                .setOnButtonClickListener("good_tag_name", null, null)
-                .setProgressBarColor(Color.WHITE)
-                .setText("Email deleted")
-                .setDuration(Style.DURATION_LONG)
-                .setFrame(Style.FRAME_LOLLIPOP)
-                .setColor(PaletteUtils.getSolidColor(PaletteUtils.MATERIAL_PURPLE))
-                .setAnimations(Style.ANIMATIONS_POP).show();
     }
 
     }
