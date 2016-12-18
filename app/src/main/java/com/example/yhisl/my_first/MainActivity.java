58901015182cf6a5dda0982f9a1981a8507dabba
YhisLara <yhlara@alumnos.ubiobio.cl>
2016-12-18 14:01:19 -0300

    method - MainActivity with implements

--- app/src/main/java/com/example/yhisl/my_first/MainActivity.java
+++ app/src/main/java/com/example/yhisl/my_first/MainActivity.java
@@ -13,20 +13,30 @@ import com.github.johnpersano.supertoasts.library.Style;
 import com.github.johnpersano.supertoasts.library.SuperActivityToast;
 import com.github.johnpersano.supertoasts.library.utils.PaletteUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
 
     private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button) findViewById(R.id.buttonMain); //casting para acceder a propiedades de un boton
+        btn = (Button) findViewById(R.id.buttonMain);
+        //casting para acceder a propiedades de un boton
         //ya que findViewById devuelve una vista y btn pide boton
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        btn.setOnClickListener(this); //this, porque se le pasa la vista que se está implementando en la clase
+        //está forma sive solo para cuando hay un solo boton
+       /* btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this , "Button Cliked Right now" , Toast.LENGTH_LONG).show();
+
             }
-        });
+        });*/
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(MainActivity.this , "Button Cliked, use implements" , Toast.LENGTH_LONG).show();
     }
 }
