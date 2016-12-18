36c30b441bc133c93117ec31daa580d8ca5e3a46
YhisLara <yhlara@alumnos.ubiobio.cl>
2016-12-18 13:37:19 -0300

    method - MainActivity with private variable,references button

--- app/src/main/java/com/example/yhisl/my_first/MainActivity.java
+++ app/src/main/java/com/example/yhisl/my_first/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.github.johnpersano.supertoasts.library.Style;
@@ -14,14 +15,18 @@ import com.github.johnpersano.supertoasts.library.utils.PaletteUtils;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn = (Button) findViewById(R.id.buttonMain); //casting para acceder a propiedades de un boton
+        //ya que findViewById devuelve una vista y btn pide boton
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this , "Button Cliked Right now" , Toast.LENGTH_LONG).show();
+            }
+        });
     }
-    public void metodoUno(View vista){
-        int a = 4;
-        Toast.makeText(MainActivity.this , "Button Cliked" , Toast.LENGTH_LONG).show();
-    }
-    }
+}
